c38e085ec643b7f4b271725716dc8fdaffe449d7
mosheliv <mosheliv@users.noreply.github.com>
2019-05-30 23:26:00 +1200

    fixed missing level

--- link_shared.sh
+++ link_shared.sh
@@ -1,4 +1,4 @@
-g++ -fPIC -c -I../../../../lib -I/usr/local/cuda/include -fpermissive yolo_api.cpp
+g++ -fPIC -c -I../../../../../lib -I/usr/local/cuda/include -fpermissive yolo_api.cpp
 ar -x ../libyolo-lib.a 
 g++ -shared *.o -o libyolo.so -L/usr/local/lib  -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_dnn -lopencv_imgcodecs -lnvinfer -lgflags -L/usr/local/cuda/lib64 -lcudart -lcudnn -lcublas -lstdc++fs -ldl -lnvinfer_plugin
 
